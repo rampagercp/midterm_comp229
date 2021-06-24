20ff048c579d0901e7195e5d45f64df3d20e5bfb
rampagercp <rampagercp@gmail.com>
2021-06-23 21:34:26 -0300

    added a new collection

--- Server/Config/app.ts
+++ Server/Config/app.ts
@@ -11,7 +11,7 @@ import mongoose, { mongo } from 'mongoose';
 // URI
 import * as DBConfig from './db';
 
-mongoose.connect(process.env.URI || DBConfig.LocalURI, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.URI || DBConfig.RemoteURI, {useNewUrlParser: true, useUnifiedTopology: true});
 
 const db = mongoose.connection; // alias for the mongoose connection
 db.on("error", function()
