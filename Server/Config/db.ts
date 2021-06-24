046acec29651cdf2dd57b3ebe33e2d53edaf467d
rampagercp <rampagercp@gmail.com>
2021-06-23 21:09:02 -0300

    MongoDB Atlas linked

--- Server/Config/db.ts
+++ Server/Config/db.ts
@@ -1,4 +1,4 @@
 export const LocalURI = "mongodb://localhost/books229";
-export const RemoteURI = "your remote host information goes here"
-export const HostName = "LocalHost";
+export const RemoteURI = "mongodb+srv://admin:aRKM39N9JqD9fgy@cluster0.aakof.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+export const HostName = "RemoteHost";
 export const Secret = "someSecret";
