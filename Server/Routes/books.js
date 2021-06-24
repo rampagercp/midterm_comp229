30815efbda2e4ed52c7d132f71f62f547dcf0e6c
rampagercp <rampagercp@gmail.com>
2021-06-24 17:59:12 -0300

    fix get add on books routes

--- Server/Routes/books.js
+++ Server/Routes/books.js
@@ -22,6 +22,7 @@ router.get('/', (req, res, next) => {
     });
 });
 router.get('/add', (req, res, next) => {
+    res.render('books/details', { title: 'Add', page: 'details', books: '' });
 });
 router.post('/add', (req, res, next) => {
 });
--- Server/Routes/books.ts
+++ Server/Routes/books.ts
@@ -28,10 +28,7 @@ router.get('/', (req, res, next) =>
 //  GET the Book Details page in order to add a new Book
 router.get('/add', (req, res, next) => {
 
-
-    /*****************
-     * ADD CODE HERE *
-     *****************/
+  res.render('books/details', { title: 'Add', page: 'details', books: '' });
 
 });
 
